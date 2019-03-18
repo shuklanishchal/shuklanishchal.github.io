3cee890c28d44dcb9b0ff8b6a24047beb549a7aa
shuklanishchal <41271033+shuklanishchal@users.noreply.github.com>
2019-03-17 18:20:18 -0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-ReactDOM.render(<h1>Calculators</h1>, document.getElementById('info'));
-class Intro extends React.Component {
-    render() {
-        return <div><p><h3><b>What does this do?</b></h3></p><p>It looks obvious, but it does a lot more than just calculate. It calculates specific things, such as the surface area and volume of a cylinder and the area and circumference of a circle. This may help best in a geometry class, or maybe even an advanced trigonometry class.</p></div>;
-    }
-}
-ReactDOM.render(<Intro />, document.getElementById('app'));
-document.getElementById('app').style="background-color: #cdcdcd; border: 2px solid #e6e6e6";
