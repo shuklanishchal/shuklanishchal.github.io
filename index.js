d92f3f035345441cd30d95599165dee0b715dc73
Nishchal Shukla <sn362438@kent.k12.wa.us>
2020-10-02 15:07:27 -0700

    Replaced and moved files

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
-ReactDOM.render(<h1>Calculators</h1>, document.getElementById('info'));
-class Intro extends React.Component {
-    render() {
-        return (
-          <div>
-            <p>
-              <h3><b>What does this do?</b></h3>
-            </p>
-            <p>
-              It looks obvious, but it does not calculate addition, subtraction, multiplication, and division of simple numbers. It calculates with specific formulas, such as the surface area and volume of a cylinder and the area and circumference of a circle. This may help best in a geometry class, or maybe even an advanced trigonometry class.
-            </p>
-          </div>
-        );
-    }
+function scrollToAnchor(aid) {
+  var divTag = $('div[id="' + aid + '"]');
+  $('html, body').animate({scrollTop: divTag.offset().top}, 1000);
 }
-ReactDOM.render(<Intro />, document.getElementById('app'));
-document.getElementById('app').style="background-color: #cdcdcd; border: 2px solid #e6e6e6";
+window.onload = function() {
+  $('#bottom').css('opacity', '0');
+  $('#linkedin').css('opacity', '0');
+  $('#github').css('opacity', '0');
+  document.getElementById('name').value = '';
+  document.getElementById('email').value = '';
+  document.getElementById('phone').value = '';
+  document.getElementById('message').value = '';
+  document.getElementById('sig').src = "signature.gif";
+  setTimeout(function(){ document.getElementById('sig').src = "signature_static.png"; $('#linkedin').animate({opacity: 1}); $('#github').animate({opacity: 1}); $('#bottom').animate({opacity: 1});}, 2290);
+}
+function home() {
+  if($(window).width() < 766) {
+    $('#toggler').click();
+  }
+  scrollToAnchor('home');
+}
+function about() {
+  if($(window).width() < 766) {
+    $('#toggler').click();
+  }
+  scrollToAnchor('about');
+}
+function portfolio() {
+  if($(window).width() < 766) {
+    $('#toggler').click();
+  }
+  scrollToAnchor('portfolio');
+}
+function contact() {
+  if($(window).width() < 766) {
+    $('#toggler').click();
+  }
+  scrollToAnchor('contact');
+}
+$(window).scroll(function() {
+  $('.navbar').fadeIn();
+});
+$('#phone').keyup(function() {
+    $(this).val($(this).val().replace(/^(\d{3})(\d{3})(\d)+$/, "($1)$2-$3"));
+});
+$(document).ready(function(){
+  $('#submit').click(function() {
+    $('#submit').submit2();
+  });
+  $("#phone").keypress(function (e) {
+    if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+      return false;
+    }
+    var curchr = this.value.length;
+    var curval = $(this).val();
+    if (curchr == 3 && curval.indexOf("(") <= -1) {
+      $(this).val("(" + curval + ")" + " ");
+    } else if (curchr == 4 && curval.indexOf("(") > -1) {
+      $(this).val(curval + ") ");
+    } else if (curchr == 5 && curval.indexOf(")") > -1) {
+      $(this).val(curval + "-");
+    } else if (curchr == 9) {
+      $(this).val(curval + "-");
+      $(this).attr('maxlength', '14');
+    }
+  });
+});
