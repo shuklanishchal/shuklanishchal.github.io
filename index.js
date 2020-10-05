30f9f2a667c245a8a0075c15d48a2b1dbe15ed57
Nishchal Shukla <sn362438@kent.k12.wa.us>
2020-10-04 17:56:29 -0700

    Removed unnecessary js

--- index.js
+++ index.js
@@ -45,9 +45,6 @@ function checkSubmit(e) {
 $(window).scroll(function() {
   $('.navbar').fadeIn();
 });
-$('#phone').keyup(function() {
-    $(this).val($(this).val().replace(/^(\d{3})(\d{3})(\d)+$/, "($1)$2-$3"));
-});
 $(document).ready(function(){
   $('#submit').click(function() {
     $('#submit').submit2();
